9867695b3389e6f591a427327572e4c965688ae9
Pablo Gallardo <juanito.gallardo_26@hotmail.com>
2022-05-06 15:30:52 -0500

    Vistas principales

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../css/nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav-container'>
+        <h1>Face Join</h1>
+        <div className='switch-container'>
+            🌙 <div className='switch'></div> 🌞
+        </div>
+    </div>
+  )
+}
+
+export default Nav
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect } from 'react'
-import { connect, createLocalTracks } from 'twilio-video'
+import React, { useState } from 'react'
+import Loby from './Loby'
+import Nav from '../components/Nav'
 
-const Home = async () => {
+import '../css/home.css'
 
-  createLocalTracks({
-    audio: true,
-    video: { width: 640 }
-  }).then(localTracks => {
-    console.log(localTracks)
-  }).then(room => {
-    console.log(`Connected to Room: ${room.name}`);
-  });
+const Home = () => {
+
+  const [login, setLogin] = useState(false)
+  const [username, setUserName] = useState("Pablo")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLogin(true)
+  }
 
   return (
-    <div id="container">
-      Home
+    <div>
+      <Nav />
+      {
+        login
+        ? <Loby username={username}/>
+        : <form onSubmit={handleSubmit} className="form-container">
+            <p className='title'>Bienvenido!!!</p>
+            <p>Ingresa un nombre: <input type="text" value={username} onChange={ e => setUserName(e.target.value)}/></p>
+            <button>Comenzar</button>
+          </form>
+      }
     </div>
   )
 }
